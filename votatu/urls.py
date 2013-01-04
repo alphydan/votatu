c8b8eb3697c3452752f35307956401b90bccfad3
Alvaro F Boirac <alphydan@gmail.com>
2013-01-04 04:11:48 +0100

    new url structure

--- votatu/urls.py
+++ votatu/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 # Votatu Views
 from votatu.views import home
@@ -13,11 +13,13 @@ urlpatterns = patterns('',
     # Examples: 
     # url(r'^$', 'votatu.views.home', name='home'),
     url(r'^$', ListaDeLeyes.as_view(), name = 'home'),
-    url(r'^votos/', include('votatu.apps.ley.url')),
-    
+    url(r'^$', ListaDeLeyes.as_view(), name = 'home'),
+
+    url(r'^ley/', include('votatu.apps.ley.url')),
+    url(r'^acerca-de/', TemplateView.as_view(template_name="acerca-de.html"), name = 'acerca-de'),
+    # url(r'^ayuda/', TemplateView.as_view(template_name="ayudanos.html"), name = 'ayuda'),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
